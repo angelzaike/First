3298bab6a614d88ec17e1a12d04c83bd1691e247
Angel Ferrer <angel@codenation.com>
2017-08-24 16:58:05 +0800

    added landing page and index page and others

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
+ def index
+  @products = Product.all
+end
+
+def landing_page
+  @products = Product.limit(3)
+  @featured_product = Product.first
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :products
   get 'static_pages/about'
 
+  get 'static_pages/index'
+
   get 'static_pages/contact'
 
   root 'static_pages#landing_page'
