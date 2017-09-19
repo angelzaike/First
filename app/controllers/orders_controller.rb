fc039e4332884ca4ee9312d878d57f7d421e8001
Angel Ferrer <angel@codenation.com>
2017-09-19 17:52:54 +0800

    added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
 before_action :authenticate_user!
+
   def index
   end
 
   def show
   end
-
 
   def new
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,12 @@ def highest_rating_comment
   comments.rating_desc.first
 end
 
+def average_rating
+  comments.average(:rating).to_f
+end
+
  validates :name, presence: true
 end
+
 
 
