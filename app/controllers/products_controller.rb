1d8cb9e3de8fd11af97b63ee34769b39759d05c7
Angel Ferrer <angel@codenation.com>
2017-09-20 16:37:33 +0800

    added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,13 +11,14 @@ class ProductsController < ApplicationController
   else
     @products = Product.all
   end
+ @products = Product.paginate(:page => params[:page], :per_page=>10)
 end
 
   # GET /products/1
   # GET /products/1.json
   def show
  @comments = @product.comments.paginate(page: params[:page], per_page: 5).order("created_at DESC")
-end
+ end
 
   # GET /products/new
   def new
