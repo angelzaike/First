9edb83bfdb30a88b0e9a90727c1c28b0aeb6e058
Angel Ferrer <angel@codenation.com>
2017-09-20 15:41:23 +0800

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,139 @@ products = Product.create([{
 	name: 'Red High Heels',
 	description: 'High heels with glossy skin.',
 	image_url: 'https://i.imgur.com/HlM7fgk.jpg',
-	price: $50.00,
+	price: '$50.00',
 	color: "Red"
 }])
-
-
+products = Product.create([{
+	name: 'Princess',
+	description: 'Black red high heels 4 inches.',
+	image_url: 'https://i.imgur.com/c9DkyE8.jpg',
+	price: '$50.00',
+	color: "Red"
+}])
+products = Product.create([{
+	name: 'Mermaid',
+	description: 'High heels with glossy skin.',
+	image_url: 'https://i.imgur.com/HlM7fgk.jpg',
+	price: '$45.00',
+	color: "Beige"
+}])
+products = Product.create([{
+	name: 'Fairy',
+	description: 'High heel shoes with shimmering dots.',
+	image_url: 'https://i.imgur.com/nXmE3ao.jpg',
+	price: '$40.00',
+	color: "Gray"
+}])
+products = Product.create([{
+	name: 'Heiress',
+	description: 'Stiletto silver design with strap.',
+	image_url: 'https://i.imgur.com/HlM7fgk.jpg',
+	price: '$45.00',
+	color: "Silver"
+}])
+products = Product.create([{
+	name: 'Duchess',
+	description: 'Stiletto straps 4 inches.',
+	image_url: 'https://i.imgur.com/bg0Tst9.jpg',
+	price: '$50.00',
+	color: "Red"
+}])
+products = Product.create([{
+	name: 'Lady',
+	description: 'High heels with glossy skin.',
+	image_url: 'https://i.imgur.com/VdfzAPp.jpg',
+	price: '$45.00',
+	color: "Yellow"
+}])
+products = Product.create([{
+	name: 'Mistress',
+	description: 'Gray 3 inches simple high heels',
+	image_url: 'https://i.imgur.com/3KWshSq.jpg',
+	price: '$35.00',
+	color: "Gray"
+}])
+products = Product.create([{
+	name: 'Empress',
+	description: 'Stiletto high heels 5 inches with strap.',
+	image_url: 'https://i.imgur.com/2GQiuN6.jpg',
+	price: '$45.00',
+	color: "Blue"
+}])
+products = Product.create([{
+	name: 'Twinkerbell',
+	description: 'Flats shimmering doll shoes.',
+	image_url: 'https://i.imgur.com/UIc4K7F.jpg',
+	price: '$40.00',
+	color: "Silver"
+}])
+products = Product.create([{
+	name: 'Red High Heels2',
+	description: 'High heels with glossy skin.',
+	image_url: 'https://i.imgur.com/HlM7fgk.jpg',
+	price: '$50.00',
+	color: "Red"
+}])
+products = Product.create([{
+	name: 'Princess2',
+	description: 'Black red high heels 4 inches.',
+	image_url: 'https://i.imgur.com/c9DkyE8.jpg',
+	price: '$50.00',
+	color: "Red"
+}])
+products = Product.create([{
+	name: 'Mermaid2',
+	description: 'High heels with glossy skin.',
+	image_url: 'https://i.imgur.com/HlM7fgk.jpg',
+	price: '$45.00',
+	color: "Beige"
+}])
+products = Product.create([{
+	name: 'Fairy2',
+	description: 'High heel shoes with shimmering dots.',
+	image_url: 'https://i.imgur.com/nXmE3ao.jpg',
+	price: '$40.00',
+	color: "Gray"
+}])
+products = Product.create([{
+	name: 'Heiress2',
+	description: 'Stiletto silver design with strap.',
+	image_url: 'https://i.imgur.com/HlM7fgk.jpg',
+	price: '$45.00',
+	color: "Silver"
+}])
+products = Product.create([{
+	name: 'Duchess2',
+	description: 'Stiletto straps 4 inches.',
+	image_url: 'https://i.imgur.com/bg0Tst9.jpg',
+	price: '$50.00',
+	color: "Red"
+}])
+products = Product.create([{
+	name: 'Lady2',
+	description: 'High heels with glossy skin.',
+	image_url: 'https://i.imgur.com/VdfzAPp.jpg',
+	price: '$45.00',
+	color: "Yellow"
+}])
+products = Product.create([{
+	name: 'Mistress2',
+	description: 'Gray 3 inches simple high heels',
+	image_url: 'https://i.imgur.com/3KWshSq.jpg',
+	price: '$35.00',
+	color: "Gray"
+}])
+products = Product.create([{
+	name: 'Empress2',
+	description: 'Stiletto high heels 5 inches with strap.',
+	image_url: 'https://i.imgur.com/2GQiuN6.jpg',
+	price: '$45.00',
+	color: "Blue"
+}])
+products = Product.create([{
+	name: 'Twinkerbell2',
+	description: 'Flats shimmering doll shoes.',
+	image_url: 'https://i.imgur.com/UIc4K7F.jpg',
+	price: '$40.00',
+	color: "Silver"
+}])
